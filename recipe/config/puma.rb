917dfa69444b2db1dc9884f3551cdb6412b8b3ed
Jian-Long Huang <huang@jianlong.org>
2020-04-06 18:43:38 +0800

    Fix sed command (cross-platform)

--- recipe/config/puma.rb
+++ recipe/config/puma.rb
@@ -1,1 +1,5 @@
-run "sed -i 's/ENV.fetch(\"RAILS_MAX_THREADS\") { 5 }/Settings.rails_max_threads/' config/puma.rb"
+if RUBY_PLATFORM =~ /darwin/
+  run "sed -i '' 's/ENV.fetch(\"RAILS_MAX_THREADS\") { 5 }/Settings.rails_max_threads/' config/puma.rb"
+else
+  run "sed -i 's/ENV.fetch(\"RAILS_MAX_THREADS\") { 5 }/Settings.rails_max_threads/' config/puma.rb"
+end
