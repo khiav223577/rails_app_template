8382ce22b00b5a0ebb89b74abf5ae00ed08de3f9
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-12-07 16:51:22 +0800

    a work around for random errors of DatabaseCleaner
    See: https://github.com/DatabaseCleaner/database_cleaner-sequel/issues/4

--- recipe/rspec/support/database_cleaner.rb
+++ recipe/rspec/support/database_cleaner.rb
@@ -10,5 +10,8 @@ RSpec.configure do |config|
 
   config.after(:each) do
     DatabaseCleaner.clean
+  rescue NoMethodError => e # See: https://github.com/DatabaseCleaner/database_cleaner-sequel/issues/4
+    next puts("Warning: catch #{e.message}") if e.message == %(undefined method `rollback' for nil:NilClass)
+    raise e
   end
 end
