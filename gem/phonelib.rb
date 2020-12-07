b01e5e7e3d8961c088a19a86de930b0c7d941017
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-12-07 16:48:59 +0800

    remove unneeded gem

--- gem/phonelib.rb
+++
@@ -1,6 +0,0 @@
-# Ruby gem for phone validation and formatting using google
-# libphonenumber library data. https://github.com/daddyz/phonelib
-gem "phonelib"
-initializer "phonelib.rb", <<~CODE
-  Phonelib.default_country = "TW"
-CODE
--- gem/pry-rails.rb
+++
@@ -1,2 +0,0 @@
-# Rails >= 3 pry initializer. https://github.com/rweng/pry-rails
-gem "pry-rails", group: :development
