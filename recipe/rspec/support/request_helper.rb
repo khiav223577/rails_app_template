8000d07352ac16201cb460e6aa46c4701e1e91aa
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-12-07 16:24:58 +0800

    Update request_helper.rb

--- recipe/rspec/support/request_helper.rb
+++ recipe/rspec/support/request_helper.rb
@@ -1,4 +1,18 @@
 module RequestHelper
+  extend RSpec::SharedContext
+
+  subject(:send_request) {}
+  subject(:response_body) do
+    send_request
+    JSON.parse(response.body)
+  end
+  subject(:response_data) { response_body["data"] }
+  subject(:response_errors) { response_body["errors"] }
+
+  def file_fixture(filename)
+    "#{Rails.root}/spec/fixtures/#{filename}"
+  end
+
   def post(uri, options = {})
     super(*request_to(uri, options))
   end
@@ -15,6 +29,10 @@ module RequestHelper
     super(*request_to(uri, options))
   end
 
+  def patch(uri, options = {})
+    super(*request_to(uri, options))
+  end
+
   def request_to(uri, params: {}, headers: {})
     api_headers = {
       "ACCEPT"      => "application/json",
@@ -26,16 +44,30 @@ module RequestHelper
     [File.join(uri), params: params, headers: api_headers]
   end
 
-  def request_params(*keys)
-    keys.map { |k| [k, send(k)] }.to_h
+  def mailer
+    ActionMailer::Base.deliveries
+  end
+
+  def response_body
+    JSON.parse(response.body)
+  end
+
+  def expect_response_header(status, content_type = "application/json")
+    send_request
+    expect(response).to have_http_status status
+    expect(response.content_type).to eq content_type
+  end
+
+  def login(user, token)
+    # For devise:
+    # @api_headers_with_auth = user.create_new_auth_token
+
+    # For token-baesd login:
+    @access_token = user.generate_access_token(token)
   end
 
   def seed_db
     Rails.application.load_seed
-  end
-
-  def mailer
-    ActionMailer::Base.deliveries
   end
 end
 
